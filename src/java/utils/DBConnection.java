23c4ecdfacdb04e520b0ba2e5be20098d4133ed1
Pham Quang Duy <duypqhe176579@fpt.edu.vn>
2025-08-01 00:30:24 +0700

    After Merge To Duc

--- src/java/utils/DBConnection.java
+++ src/java/utils/DBConnection.java
@@ -13,9 +13,9 @@ import java.sql.SQLException;
  * @author Admin
  */
 public class DBConnection {
-    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=systemhouse4;encrypt=true;trustServerCertificate=true";
+    private static final String URL = "jdbc:sqlserver://localhost:1433;databaseName=systemhouse1;encrypt=true;trustServerCertificate=true";
     private static final String USERNAME = "sa"; 
-    private static final String PASSWORD = "123"; 
+    private static final String PASSWORD = "sa"; 
     
     static {
         try {
